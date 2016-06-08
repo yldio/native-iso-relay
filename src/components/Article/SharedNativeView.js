882b02d21d3cb8a3da4fd186e6e5fa6c617db4d3
Ericat <erica.salvaneschi@gmail.com>
2016-06-08 10:56:07 +0100

    refactor shared view into pure function component

--- src/components/Article/SharedNativeView.js
+++ src/components/Article/SharedNativeView.js
@@ -5,43 +5,35 @@ import {
     Image,
 } from 'react-native';
 
-class SharedNativeView extends React.Component {
-  static propTypes = {
-    article: React.PropTypes.arrayOf(
-      React.PropTypes.shape({
-        id: React.PropTypes.string,
-        title: React.PropTypes.string,
-        author: React.PropTypes.string,
-        description: React.PropTypes.string,
-        text: React.PropTypes.string,
-        mainImage: React.PropTypes.string,
-        pubdate: React.PropTypes.string,
-      })
-    ),
-    styles: React.PropTypes.shape({}),
-  };
+function SharedNativeView(props) {
+  const { article, styles } = props;
+  const articleBody = article[0];
 
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  render() {
-    const { article, styles } = this.props;
-    const articleBody = article[0];
-
-    return(
-      <View style={[styles.mainContainer, styles.mainArticleContainer]}>
-        <Text style={styles.title}>{articleBody.title}</Text>
-        <Text style={styles.description}>{articleBody.description}</Text>
-        <Text style={styles.author}>{articleBody.author}</Text>
-        <Image
-          source={{ uri: articleBody.mainImage }}
-          style={styles.mainImage}
-        />
-        <Text style={styles.body}>{articleBody.text}</Text>
-      </View>
-    );
-  }
+  return(
+    <View style={[styles.mainContainer, styles.mainArticleContainer]}>
+      <Text style={styles.title}>{articleBody.title}</Text>
+      <Text style={styles.description}>{articleBody.description}</Text>
+      <Text style={styles.author}>{articleBody.author}</Text>
+      <Image
+        source={{ uri: articleBody.mainImage }}
+        style={styles.mainImage}
+      />
+      <Text style={styles.body}>{articleBody.text}</Text>
+    </View>
+  );
 }
+
+SharedNativeView.propTypes = {
+  article: React.PropTypes.shape({
+    id: React.PropTypes.string,
+    title: React.PropTypes.string,
+    author: React.PropTypes.string,
+    description: React.PropTypes.string,
+    text: React.PropTypes.string,
+    mainImage: React.PropTypes.string,
+    pubdate: React.PropTypes.string,
+  }).isRequired,
+  styles: React.PropTypes.shape({}).isRequired,
+};
 
 export default SharedNativeView;
